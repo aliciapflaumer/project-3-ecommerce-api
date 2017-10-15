0ef46f351fb1eeee6e34a8db96661b821eefccf7
stgeorge3 <amaxmazzone@gmail.com>
2017-10-15 12:59:55 -0400

    Add authenticate to index and show in orders controller
    Adds authentication so you need to be logged in to see orders.
    Adjusts the index method to only show orders that the logged in user
    has made.
    
    Contributors:
    @aliciapflaumer
    @stgeorge3

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -9,7 +9,7 @@ const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
 
 const index = (req, res, next) => {
-  Order.find()
+  Order.find({ _owner: req.user })
     .then(orders => res.json({
       orders: orders.map((e) =>
         e.toJSON({ virtuals: true, user: req.user }))
@@ -80,7 +80,7 @@ module.exports = controller({
   destroy
 }, { before: [
   { method: setUser, only: ['index', 'show'] },
-  { method: authenticate, except: ['index', 'show'] },
+  { method: authenticate },
   { method: setModel(Order), only: ['show'] },
   { method: setModel(Order, { forUser: true }), only: ['update', 'destroy'] }
 ] })
